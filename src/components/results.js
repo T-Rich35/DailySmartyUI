c2efe037b8d53b1d9795e1b16b84225799081b16
Tarrance <richistee@gmail.com>
2022-10-05 22:00:46 -0500

    add results component

---
+++ src/components/results.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Logo from "./logo";
+
+class Results extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello buddy</h1>
+        <Logo />
+      </div>
+    );
+  }
+}
+export default Results;
