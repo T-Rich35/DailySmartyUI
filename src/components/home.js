71a1c1bd520ce2b8b953fc10f8ef5a5014eb824d
Tarrance <richistee@gmail.com>
2022-10-06 22:46:44 -0500

    fixed onSubmit code error

--- src/components/home.js
+++ src/components/home.js
@@ -4,14 +4,24 @@ import Logo from "./logo";
 import SearchBar from "./SearchBar";
 import RecentPosts from "./recentPosts";
 
-export default class Home extends Component {
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Home extends Component {
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
+    this.props.history.push("/results");
+  }
+
   render() {
     return (
       <div>
         <Logo />
-        <SearchBar />
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
         <RecentPosts />
       </div>
     );
   }
 }
+
+export default connect(null, actions)(Home);
--- src/components/results.js
+++ src/components/results.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import Logo from "./logo";
 import SearchBar from "./SearchBar";
 
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
 class Results extends Component {
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
+  }
+
   render() {
     return (
       <div>
         <Logo size={55} />
-        <SearchBar />
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
       </div>
     );
   }
 }
-export default Results;
+export default connect(null, actions)(Results);
